188ba0faefa62950c8132d195b8e184896e04dca
dilan <dilan.silva@colombo.rezgateway.com>
2022-12-30 18:54:26 +0530

    SOme changes

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,103 @@
-import { Image, StyleSheet,TextInput ,Text, View,TouchableOpacity } from 'react-native';
+import { Image, StyleSheet,TextInput ,Text, View,TouchableOpacity,Modal,Pressable } from 'react-native';
+import { Avatar } from '@rneui/themed';
+import { useEffect, useState } from 'react';
 
-const Home = () => {
+const Home = ({navigation}) => {
+    const [modalVisible, setModalVisible] = useState(false);
     return (
-        <View>
-            <Text>Home Page</Text>
+        <View style={{flex:1,backgroundColor: '#111111'}}>
+            <View style={{marginLeft:20,flexDirection:'row'}}>
+               <View>
+                    <Text style={{fontSize:35,fontWeight:'bold',color:'white'}}>Good Afternoon</Text>
+                        <Text style={{fontSize:15,fontWeight:'bold',color:'white'}}>Recently Played</Text>
+               </View>
+               <View style={{marginLeft:70,marginTop:10}}> 
+                    <Avatar
+                        size={60}
+                        rounded
+                        source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
+                        onPress={() => setModalVisible(true)}
+                    />
+               </View>
+               <View>
+                    <Image
+                        source={require('../assets/practical_test_logo.png')}
+                    />
+               </View>
+            </View>
+
+
+            <View style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Logging Out</Text>
+                        <Text style={{marginBottom:10}}>Are you sure want to log out?</Text>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => navigation.navigate('Login')}
+                        >
+                        <Text style={styles.textStyle}>Log Out</Text>
+                        </Pressable>
+                        
+                    </View>
+                    </View>
+                </Modal>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    buttonOpen: {
+      backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+  });
 
 export default Home;
---
+++ screens/Library.js
@@ -0,0 +1,11 @@
+import { Image, StyleSheet,TextInput ,Text, View,TouchableOpacity } from 'react-native';
+
+const Library = () => {
+    return (
+        <View>
+            <Text>Library Page</Text>
+        </View>
+    );
+}
+
+export default Library;
